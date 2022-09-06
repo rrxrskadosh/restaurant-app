e6269776fcb62e08d240e961845755d0744b7325
rrxrskadosh <kadoshrr2019@gmail.com>
2022-09-05 21:01:19 -0500

    Installed and Config TailwindCSS, Resetting CSS, Added Directives TailwindCSS, Added Styles Tailwind in App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
     return(
         <>
-            <h1 className="App">Hello Restaurant App</h1>
+            <h1 className="w-screen h-screen flex justify-center item-center text-blue-600">
+                Hello Restaurant App
+            </h1>
         </>
     )
 }
