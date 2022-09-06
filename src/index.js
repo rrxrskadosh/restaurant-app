d999c56da707c6cbee02db804412959078b193a8
rrxrskadosh <kadoshrr2019@gmail.com>
2022-09-05 21:25:49 -0500

    Modifying index.html, App.js and index.js - Fixed sintaxis and deleted comments useless

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React, { StrictMode } from "react";
-import ReactDOM from "react";
-import './index.css';
+import ReactDOM from "react-dom";
+import "./index.css";
 
-import App from './App';
+import App from "./App";
 
 ReactDOM.render(
     <StrictMode>
-        <App />
-    </StrictMode>, document.getElementById("root")
+         <App />
+    </StrictMode>, document.getElementById('root')
 ); 
 
