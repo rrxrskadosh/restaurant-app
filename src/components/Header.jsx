d9610d91715e6d422697932ba637b2f4e04851d7
rrxrskadosh <kadoshrr2019@gmail.com>
2022-09-25 15:12:04 -0500

    Fixed elements navbar mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -111,6 +111,14 @@ const Header = () => {
 
             {/* mobile */}
             <div className="flex items-center justify-between md:hidden w-full h-full">
+                <div className="relative flex items-center justify-center">
+                    <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
+                    <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg
+                    flex items-center justify-center">
+                        <p className="text-xs text-white font-semibold">2</p>
+                    </div>
+                </div>
+
                 <Link to={"/"} className="flex items center gap-2">
                     <img src={Logo} className="w-20 object-cover cursor-pointer" alt="logo"/>
                     <p className="text-cartNumBg text-xl font-bold mt-4">RestauApp</p>
