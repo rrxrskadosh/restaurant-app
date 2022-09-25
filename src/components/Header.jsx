67ace31bcb3c58c8a47ddf3c43b79e664a81bd3a
rrxrskadosh <kadoshrr2019@gmail.com>
2022-09-25 15:01:59 -0500

    Building method logout and event onClick to signout Firebase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,6 +35,16 @@ const Header = () => {
        }
     }
 
+    const logout = () => {
+        setIsMenu(false);
+        localStorage.clear();
+
+        dispatch({
+            type: actionType.SET_USER,
+            user: null
+        });
+    };
+
     return(
         <header className="fixed z-50 w-screen p-4 px-8 md:p-6 md:px-10">
             {/* desktop & tablet */}
@@ -89,7 +99,7 @@ const Header = () => {
                             
                                 <p className="px-4 py-2 flex items-center gap-3 
                                     cursor-pointer hover:bg-slate-100 transition-all duration-100 
-                                    ease-in-out text-textColor text-base">Logout <MdLogout />
+                                    ease-in-out text-textColor text-base" onClick={logout}>Logout <MdLogout />
                                 </p>
                             </motion.div>
                             )
@@ -137,9 +147,9 @@ const Header = () => {
                                     <li className="text-base text-textColor hover:text-headingColor duration-75 transition-all ease-in-out cursor-pointer">Service</li>
                                 </ul>
                             
-                                <p className="px-4 py-2 flex items-center gap-3 
-                                    cursor-pointer hover:bg-slate-100 transition-all duration-100 
-                                    ease-in-out text-textColor text-base">Logout <MdLogout />
+                                <p className="px-4 py-2 flex items-center justify-center gap-3 
+                                    cursor-pointer bg-gray-200 hover:bg-gray-300 transition-all duration-100 
+                                    ease-in-out text-textColor text-base" onClick={logout}>Logout <MdLogout />
                                 </p>
                             </motion.div>
                             )
